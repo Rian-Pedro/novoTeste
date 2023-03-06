c98fd76b9b56b5a3e73f74d398dd16e8caaf0a26
Rian <rian.ace.2003@gmail.com>
2023-03-06 20:04:40 -0300

    ...

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post('/img', uploadImage.single('foto'), (req, res) =>{
   res.send('Foto armazenada com sucesso')
 })
 app.get('/img', (req, res) => {
-  res.send('teste')
+  res.sendFile(__dirname + '/img/' + 'teste.png')
 })
 
 const port = process.env.PORT || 9001
