9e2b9be0960613a0c95a01382ec0d2d6189b9483
Rian <rian.ace.2003@gmail.com>
2023-03-06 19:52:30 -0300

    ...

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const home = require('./routes/home')
-const routes = require('./routes/uploadImg')
+const uploadImage = require('./middleware/uploadImage')
 
 const app = express()
 app.use(express.json())
 
 app.use('/home', home)
-app.use(routes)
+app.post('/img', uploadImage.single('foto'), (req, res) =>{
+  res.send('Foto armazenada com sucesso')
+})
+app.get('/img', (req, res) => {
+  res.send('teste')
+})
 
 const port = process.env.PORT || 9001
 app.listen(port)
--- routes/uploadImg.js
+++ routes/uploadImg.js
@@ -1,15 +1,5 @@
-const router = require('express').Router()
-const cors = require('cors')
 const uploadImg = require('../middleware/uploadImage')
 
-router.post('/img', uploadImg.single('foto'), (req, res) => {
-  cors()
+module.exports = (req, res) => {
   res.send('Arquivo salvo com sucesso')
-});
-
-router.get('/img', (req, res) => {
-  cors()
-  res.sendFile(__dirname + '/img/' + 'teste.png')
-})
-
-module.exports = router
+};
