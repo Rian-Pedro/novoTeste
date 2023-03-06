b8a9ed81e380b06a1524ea1b198adb108fdc74aa
Rian <rian.ace.2003@gmail.com>
2023-03-06 19:47:47 -0300

    ...

--- routes/uploadImg.js
+++ routes/uploadImg.js
@@ -1,11 +1,14 @@
 const router = require('express').Router()
+const cors = require('cors')
 const uploadImg = require('../middleware/uploadImage')
 
 router.post('/img', uploadImg.single('foto'), (req, res) => {
+  cors()
   res.send('Arquivo salvo com sucesso')
 });
 
 router.get('/img', (req, res) => {
+  cors()
   res.sendFile(__dirname + '/img/' + 'teste.png')
 })
 
